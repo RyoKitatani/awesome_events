2bb44e63bbf4f17ff56b6656c04b75b1b540fa7e
RyoKitatani <ryo0929m@gmail.com>
2021-08-01 16:43:45 +0900

    add_event_edit&update_action

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,12 @@ class Event < ApplicationRecord
   validates :start_at, presence: true
   validates :end_at, presence: true
   validate :start_at_should_be_before_end_at
-  
+
+  def created_by?(user)
+    return false unless user
+    owner_id == user.id
+  end
+
   private
 
   def start_at_should_be_before_end_at
