7d04c369a305c75753492c18c2e7b2221b26a40b
RyoKitatani <ryo0929m@gmail.com>
2021-08-01 14:24:48 +0900

    fix_event_new_page

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module AwesomeEvents
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
   end
 end
