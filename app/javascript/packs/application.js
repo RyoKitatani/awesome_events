44dd1fcfbbd1286f1bc10285927edd842c9d08a2
RyoKitatani <ryo0929m@gmail.com>
2021-08-01 10:20:24 +0900

    add_bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,12 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import Bootstarp from "bootstrap"
+import Bootstarp_scss from "bootstrap/scss/bootstrap.scss"
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+Bootstarp.start()
+Bootstarp_scss.start()
