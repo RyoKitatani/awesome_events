9f4c3571d87c119e09797f652b29629a04ddc3c2
RyoKitatani <ryo0929m@gmail.com>
2021-08-06 21:21:46 +0900

    add_event_test_delete_check

--- test/sign_in_helper.rb
+++ test/sign_in_helper.rb
@@ -1,4 +1,4 @@
-module SignHelper
+module SignInHelper
   def sign_in_as(user)
     OmniAuth.config.test_mode = true
     OmniAuth.config.add_mock(
@@ -25,6 +25,6 @@ module SignHelper
   end
 end
 
-class ActionDispatch::IntergrationTest
+class ActionDispatch::IntegrationTest
   include SignInHelper
 end
